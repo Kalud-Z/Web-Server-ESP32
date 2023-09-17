e3d7f2776c88d5ffb13d7a82d71f4aa1ad575815
Khaled Fahem <khaled.fhm@live.fr>
2023-09-17 20:13:43 +0200

    first commit. wifi works.

---
+++ main/wifi_manager.h
@@ -0,0 +1,11 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#include "esp_err.h"
+
+// Function to initialize WiFi as station mode and connect to the defined SSID.
+void wifi_init_sta(void);
+
+extern const char *TAG;  // External declaration for the TAG variable
+
+#endif // WIFI_MANAGER_H
