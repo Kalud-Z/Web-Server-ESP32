5e5a858090393e2233c46d8a1e7707da04ebf1f4
Khaled Fahem <khaled.fhm@live.fr>
2023-11-28 14:42:59 +0100

    ws works. next : implement the logic of data generator

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -10,11 +10,14 @@
 #include "esp_netif.h"
 #include "esp_spiffs.h"
 #include "esp_vfs.h"
+#include "esp_random.h"  // Include for random number generation
 
 
 
 
 const char *TAG_MAIN = "main";
+httpd_handle_t server = NULL; // Declare the server handle globally
+
 
 
 void hello_world_task(void *pvParameter) {
@@ -25,48 +28,55 @@ void hello_world_task(void *pvParameter) {
 
         ESP_LOGI(TAG_MAIN, "Goodbye world 5555");
 
-        vTaskDelay(1000 / 5); 
+        vTaskDelay(10000 / 5); 
     }
 }
 
 
+// Function to generate random numbers
+int generate_random_number() {
+    return esp_random() % 4001 + 1000; // Generate random number between 1000 and 5000
+}
 
-static esp_err_t ws_handler(httpd_req_t *req)
-{
-    uint8_t buf[128] = { 0 };
+
+static esp_err_t ws_handler(httpd_req_t *req) {
+    ESP_LOGI(TAG_MAIN, "WebSocket Connection Started");
+
+    char num_str[10];
     httpd_ws_frame_t ws_pkt;
     memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    ws_pkt.payload = buf;
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
 
-    // Receive the WebSocket message
-    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 128);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG_MAIN, "Error receiving the frame");
-        return ESP_FAIL;
+    for (int i = 0; i < 30; i++) { // Send 10 numbers as an example
+        int random_number = generate_random_number();
+        sprintf(num_str, "%d", random_number); // Convert number to string
+        ws_pkt.payload = (uint8_t *)num_str;
+        ws_pkt.len = strlen(num_str);
+
+        esp_err_t ret = httpd_ws_send_frame(req, &ws_pkt);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG_MAIN, "Error sending frame: %s", esp_err_to_name(ret));
+            return ESP_FAIL;
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
     }
 
-    // Handle the WebSocket message (echo in this case)
-    if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
-        ws_pkt.len < 128) {
-        buf[ws_pkt.len] = '\0';  // Null-terminate the received string
-        ESP_LOGI(TAG_MAIN, "WebSocket Message: %s", buf);
-        // Echo the message back
-        httpd_ws_send_frame(req, &ws_pkt);
-    }
-
+    ESP_LOGI(TAG_MAIN, "WebSocket Connection Closed");
     return ESP_OK;
 }
+
 
 
 
 void start_webserver(void)
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.server_port = 8999; // Set the port to 8999
 
     // Start the httpd server
     httpd_handle_t server = NULL;
     if (httpd_start(&server, &config) == ESP_OK) {
-        ESP_LOGI(TAG_MAIN, "Server started");
+        ESP_LOGI(TAG_MAIN, "Server started on port %d", config.server_port);
 
         // URI for WebSocket
         httpd_uri_t ws_uri = {
@@ -108,7 +118,7 @@ void app_main(void)
 
     while (1) {
         if (wifi_connected_flag) {
-            //start_webserver();
+            start_webserver();
             break; 
         }
         vTaskDelay(pdMS_TO_TICKS(2000));
