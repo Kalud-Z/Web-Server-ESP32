2afd10fd8d6c972c087d1775ac10d3debd6e6327
Khaled Fahem <khaled.fhm@live.fr>
2023-11-28 13:14:31 +0100

    facing alot of build issues. next trying mongoose library

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -14,34 +14,7 @@
 
 
 const char *TAG_MAIN = "main";
-
-
-const char *data = 
-"87780270000ns 751 555\n"
-"87800417000ns 750 557\n"
-"87820270000ns 747 555\n"
-"87840456000ns 752 560\n"
-"87860609000ns 749 553\n"
-"87880405000ns 749 554\n"
-"87900552000ns 753 555\n"
-"87920694000ns 751 559\n"
-"87940495000ns 752 556\n"
-"87960642000ns 751 555\n"
-"87980783000ns 746 553\n"
-"88000933000ns 750 557\n"
-"88020784000ns 751 555\n"
-"88040973000ns 749 558\n"
-"88060783000ns 751 559\n"
-"88080932000ns 750 553\n"
-"88101078000ns 749 560\n"
-"88120882000ns 752 558\n"
-"88141023000ns 749 556\n"
-"88161171000ns 751 558\n"
-"88181312000ns 750 553\n"
-"88201118000ns 747 558\n";
-
-
-
+const char *TAG_ws = "ws_server";
 
 
 void hello_world_task(void *pvParameter) {
@@ -50,92 +23,54 @@ void hello_world_task(void *pvParameter) {
             ESP_LOGI(TAG_MAIN, "IP Address: " IPSTR, IP2STR(&global_ip_address));
         }
 
-        ESP_LOGI(TAG_MAIN, "Goodbye world 33333");
+        ESP_LOGI(TAG_MAIN, "Goodbye world 44444");
 
         vTaskDelay(1000 / 5); 
     }
 }
 
 
-const char* get_line(const char* data, int line_number) {
-    static char buffer[256];
-    char data_copy[1024]; // Assuming 1024 is enough
-    strncpy(data_copy, data, sizeof(data_copy) - 1);
-    data_copy[sizeof(data_copy) - 1] = '\0'; // Null-terminate just in case
 
-    int current_line = 0;
-    char* token = strtok(data_copy, "\n");
-    while (token) {
-        if (current_line == line_number) {
-            strncpy(buffer, token, sizeof(buffer) - 1);
-            buffer[sizeof(buffer) - 1] = '\0'; // Ensure null-termination
-            return buffer;
-        }
-        token = strtok(NULL, "\n");
-        current_line++;
+
+
+/* An example of WebSocket handler */
+static esp_err_t websocket_handler(httpd_req_t *req) {
+    if (req->method == HTTP_GET) {
+        ESP_LOGI(TAG_ws, "Handshake done, WebSocket connection established");
+        const char *response = "connection established :)";
+        httpd_ws_frame_t ws_response = {
+            .payload = (uint8_t *)response,
+            .len = strlen(response),
+            .type = HTTPD_WS_TYPE_TEXT,
+            .final = true,
+            .fragmented = false,  // Set fragmented to false for a complete message
+        };
+        httpd_ws_send_frame(req, &ws_response);
+        return ESP_OK;
     }
-    return NULL; // Line not found
+    // Handle other WebSocket events here
+    return ESP_FAIL;
 }
-
-
-
-esp_err_t hello_get_handler(httpd_req_t *req)
-{
-    ESP_LOGI(TAG_MAIN, "hello_get_handler just called._______________________________");  
-
-    char response[1024] = {0};
-    for (int i = 0; i < 10; i++) {
-        const char *line = get_line(data, i);
-        if(line) {
-            strncat(response, line, sizeof(response) - strlen(response) - 1);
-            strncat(response, "\n", sizeof(response) - strlen(response) - 1);
-        }
-    }
-    
-    ESP_LOGI(TAG_MAIN, "this is response sent to client : %s", response);  
-
-    httpd_resp_send(req, response, strlen(response));
-    return ESP_OK;
-}
-
-
-// esp_err_t hello_get_handler(httpd_req_t *req)
-// {
-//     ESP_LOGI(TAG_MAIN, "hello_get_handler just called.");  
-
-//     const char* resp_str = "This is your response :)";
-
-//     ESP_LOGI(TAG_MAIN, "this is response sent to client : %s", resp_str);  
-
-//     httpd_resp_send(req, resp_str, strlen(resp_str)); 
-//     return ESP_OK;
-// }
 
 
 
 void start_webserver(void)
 {
-    httpd_handle_t server = NULL;
+  httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
-    // Start the HTTP server
+    // Start the httpd server
+    ESP_LOGI(TAG_ws, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
-        // Set URI handlers
-        httpd_uri_t hello_uri = {
-            .uri       = "/hello",
+        // Register the WebSocket handler
+        httpd_uri_t ws_uri = {
+            .uri       = "/ws", // The WebSocket endpoint
             .method    = HTTP_GET,
-            .handler   = hello_get_handler,
+            .handler   = websocket_handler,
             .user_ctx  = NULL
         };
-        httpd_register_uri_handler(server, &hello_uri);
-    }
-
-    // Print server status
-    if (server) {
-        ESP_LOGI(TAG_MAIN, "HTTP server started on port %d", config.server_port);
-    } else {
-        ESP_LOGE(TAG_MAIN, "Failed to start HTTP server!");
-    }
+        httpd_register_uri_handler(server, &ws_uri);
+    }   
 }
 
 
@@ -148,23 +83,6 @@ void init_NVS() {
     }
     ESP_ERROR_CHECK(ret);
 }
-
-
-// void init_spiffs() {
-//     esp_vfs_spiffs_conf_t conf = {
-//       .base_path = "/spiffs",
-//       .partition_label = NULL,
-//       .max_files = 5,
-//       .format_if_mount_failed = true
-//     };
-
-//     esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-//     if (ret != ESP_OK) {
-//         ESP_LOGE(TAG_MAIN, "Failed to initialize SPIFFS");
-//         return;
-//     }
-// }
 
 
 
@@ -181,7 +99,7 @@ void app_main(void)
 
     while (1) {
         if (wifi_connected_flag) {
-            start_webserver();
+            //start_webserver();
             break; 
         }
         vTaskDelay(pdMS_TO_TICKS(2000));
