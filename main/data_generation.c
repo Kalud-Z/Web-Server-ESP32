668ee6cf812fc432d798a2553475b5d7f91ec145
Khaled Fahem <khaled.fhm@live.fr>
2023-11-30 20:14:47 +0100

    now sine wave is plotted

--- main/data_generation.c
+++ main/data_generation.c
@@ -6,13 +6,20 @@
 #include <stdlib.h>          // For malloc and NULL
 #include <stdint.h>          // For fixed-width integer types
 #include <string.h>
-
+#include <math.h>            // Include the math library for sin function
 
 
 static const char *TAG = "DataGeneration";
 
 
-uint8_t* generate_data_batch(uint32_t batchCount, size_t *binary_size_out, int dataPointsPerBatch) {    
+
+// Helper function to generate sine wave data
+static uint32_t generate_sine_data(double angle, uint32_t min, uint32_t max) {
+    double sine_value = sin(angle);
+    return (uint32_t)(((sine_value + 1) / 2) * (max - min) + min);
+}
+
+uint8_t* generate_data_batch(uint32_t batchCount, size_t *binary_size_out, int dataPointsPerBatch) {
     DataBatch batch;
     batch.batchID = batchCount;
     *binary_size_out = 4 + (dataPointsPerBatch * sizeof(DataPoint));
@@ -24,23 +31,32 @@ uint8_t* generate_data_batch(uint32_t batchCount, size_t *binary_size_out, int d
 
     uint8_t* ptr = binary_data;
     memcpy(ptr, &batch.batchID, 4); ptr += 4;
-    uint64_t currentTime = esp_timer_get_time();
+
+    // Constants for the sine wave generation
+    const uint32_t min = 10000000;
+    const uint32_t max = 16777215;
+    const int totalPointsInPeriod = 1000; // Points in one period of the sine wave
+
+    static uint32_t dataPointId = 0; // Static variable to keep track across function calls
+    static uint32_t totalDataPointsGenerated = 0; // Static variable to keep track across function calls
 
     for (int i = 0; i < dataPointsPerBatch; i++) {
+        totalDataPointsGenerated++; // Increment total data points generated
+
         DataPoint point;
-        point.timestamp = currentTime + (i * 1000);
+        point.timestamp = esp_timer_get_time();
 
-        // Generate random values for channel1Value and channel2Value
-        point.channel1Value = 10000000 + esp_random() % (16777215 - 10000000 + 1);
-        point.channel2Value = 10000000 + esp_random() % (16777215 - 10000000 + 1);
-        
-        point.dataPointID = (batchCount - 1) * dataPointsPerBatch + i + 1;
-        //ESP_LOGI(TAG, "Generated dataPointID: %u", point.dataPointID);
-        //ESP_LOGI(TAG, "Generated dataPointID: %lu", (unsigned long)point.dataPointID);
+        // Calculate the sine wave values
+        double angle = (2 * M_PI * (totalDataPointsGenerated % totalPointsInPeriod)) / totalPointsInPeriod;
+        point.channel1Value = generate_sine_data(angle, min, max);
+        point.channel2Value = generate_sine_data(angle, min, max);
 
+        point.dataPointID = dataPointId++;
 
         memcpy(ptr, &point, sizeof(DataPoint)); ptr += sizeof(DataPoint);
     }
 
     return binary_data;
 }
+
+
