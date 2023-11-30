1ef4df04522ae83d70659e8cbcfa8f25aa618441
Khaled Fahem <khaled.fhm@live.fr>
2023-11-30 21:23:14 +0100

    almost eveything is ready. NEXT: send timestamps to the client  when simulation is done

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -22,23 +22,23 @@
 
 
 
+int64_t durationOfSimulation = 30000000; // 30 seconds in microseconds
+const int numberOfChannels = 5;
+const int dataPointsPerBatch = 20;
+const int sampleRate = 20;
+
+
+
+
 const char *TAG_MAIN = "main";
 
-const int numberOfChannels = 5;
-
-const int totalDataPoints = 10000;
-const int dataPointsPerBatch = 10;
-int sampleRate = 10;
-
-
 httpd_handle_t server = NULL; // Declare the server handle globally
-
 
 
 static esp_err_t ws_handler(httpd_req_t *req) {
     ESP_LOGI(TAG_MAIN, "WebSocket Connection Started");
 
-     // Declare the init_message with enough size to hold the longest possible string
+    // Send initial configuration message
     char init_message[64];
     snprintf(init_message, sizeof(init_message), "{ \"type\": \"configuration\", \"channels\": %d }", numberOfChannels);
     httpd_ws_frame_t init_frame;
@@ -52,62 +52,48 @@ static esp_err_t ws_handler(httpd_req_t *req) {
         ESP_LOGE(TAG_MAIN, "Error sending initial frame: %s", esp_err_to_name(ret));
         return ESP_FAIL;
     }
-    
-    const int totalBatches = totalDataPoints / dataPointsPerBatch;
 
-    for (uint32_t batchCount = 1; batchCount <= totalBatches; batchCount++) {
-        int64_t startTime = esp_timer_get_time();  // Start time in microseconds
+    // Start sending data batches for a specified duration
+    int64_t startSendingTime = esp_timer_get_time(); // Start time in microseconds
+    uint32_t batchCount = 0; // Initialize batch count
+
+    while (esp_timer_get_time() - startSendingTime < durationOfSimulation) {
+        batchCount++; // Increment batch count
 
         size_t binary_size;
         uint8_t* binary_data = generate_data_batch(batchCount, &binary_size, dataPointsPerBatch, numberOfChannels);
-        int64_t endTime = esp_timer_get_time();  // End time in microseconds
-        int64_t generationTime = (endTime - startTime) / 1000;  // Convert to milliseconds
 
         if (binary_data == NULL) {
             return ESP_FAIL;
         }
 
-        // Log the total size of the batch being sent
-        //ESP_LOGI(TAG_MAIN, "Sending batchID: %" PRIu32 " | Size: %zu bytes", batchCount, binary_size);
-
-
-        // Get the current time
+        // Log the timestamp
         struct timeval tv_now;
         gettimeofday(&tv_now, NULL);
         struct tm *tm_now = localtime(&tv_now.tv_sec);
-
-        // Format the time
         char strftime_buf[64];
         strftime(strftime_buf, sizeof(strftime_buf), "%H:%M:%S", tm_now);
-
-        // Log the timestamp
         ESP_LOGI(TAG_MAIN, "Dispatching batchID: %" PRIu32 " | timestamp: %s:%03ld", batchCount, strftime_buf, tv_now.tv_usec / 1000);
 
-
+        // Send the data batch
         httpd_ws_frame_t ws_pkt;
         memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
         ws_pkt.type = HTTPD_WS_TYPE_BINARY;
         ws_pkt.payload = binary_data;
         ws_pkt.len = binary_size;
 
-        esp_err_t ret = httpd_ws_send_frame(req, &ws_pkt);
+        ret = httpd_ws_send_frame(req, &ws_pkt);
         free(binary_data);
+
         if (ret != ESP_OK) {
             ESP_LOGE(TAG_MAIN, "Error sending frame: %s", esp_err_to_name(ret));
             return ESP_FAIL;
         }
 
-         if (generationTime > sampleRate) {
-            ESP_LOGE(TAG_MAIN, "$$$$$Data generation took longer than target interval: %" PRId64 " ms", generationTime);
-            // Handle the error condition here (e.g., skip sending this batch, reset, etc.)
-            continue;  // Skip to the next iteration, or use a different handling approach
-        }
-
-        int finalDelayTime = sampleRate - generationTime;
-        vTaskDelay(pdMS_TO_TICKS(finalDelayTime)); 
+        vTaskDelay(pdMS_TO_TICKS(sampleRate)); 
     }
 
-    // After all data has been sent, send a final structured message to indicate completion
+    // Send end of simulation message
     char done_message[64];
     snprintf(done_message, sizeof(done_message), "{ \"type\": \"simulationState\", \"value\": \"DONE\" }");
     httpd_ws_frame_t done_frame;
@@ -116,17 +102,17 @@ static esp_err_t ws_handler(httpd_req_t *req) {
     done_frame.payload = (uint8_t*)done_message;
     done_frame.len = strlen(done_message);
 
-    esp_err_t ret2 = httpd_ws_send_frame(req, &done_frame);
-    if (ret2 != ESP_OK) {
-        ESP_LOGE(TAG_MAIN, "Error sending done frame: %s", esp_err_to_name(ret2));
+    ret = httpd_ws_send_frame(req, &done_frame);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG_MAIN, "Error sending done frame: %s", esp_err_to_name(ret));
         // Handle error appropriately
     }
 
-
-
-    //ESP_LOGI(TAG_MAIN, "WebSocket Connection Closed");
     return ESP_OK;
 }
+
+
+
 
 
 
